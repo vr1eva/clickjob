7b1a0c1dcf1e3b3ecca7c7cbfdce8002b1aa3dfc
vr1 <113930888+vr1eva@users.noreply.github.com>
2023-12-21 11:58:19 -0500

    Created jobs page (#4)
    * Created jobs page
    
    * Add empty line to the end of file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
+import Link from "next/link"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Link href="/jobs">Jobs</Link>
+          {children}</body>
       </html>
     </ClerkProvider>
   )
